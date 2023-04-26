31e2f674f1ba573ba66b23eb4d203a008af6a065
DanieleMorello <danielemorello@live.it>
2023-04-26 17:59:46 +0200

    Add Header

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,9 +1,44 @@
 <script>
+import { library } from '@fortawesome/fontawesome-svg-core'
 export default {
   name: 'PageHeader'
 }
 </script>
 
-<template></template>
+<template>
+  <header>
+    <nav>
+      <div>
+        <img src="../assets/img/author-logo-round-small.png" alt="Logo">
+      </div>
+
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About Me</a></li>
+        <li><a href="#">Testimonials</a></li>
+        <li><a href="#">My Blog</a></li>
+        <li><a href="#">Meetups</a></li>
+        <li><a href="#">Shop</a></li>
+        <li><a href="#">Contact Me</a></li>
+        <li><a href="#"><font-awesome-icon :icon="['fas', 'cart-shopping']" /></a></li>
+      </ul>
+    </nav>
+
+    <div class="container">
+      <section class="my_container">
+        <h1>Damon Vaughn</h1>
+        <h2>
+          Best-selling author and the most <br>
+          influential public intellectual in the <br>
+          avoctors world richt now.
+        </h2>
+        <p>-The New York Times</p>
+        <button class="left border-0 rounded-1"><font-awesome-icon :icon="['fas', 'arrow-left']" /></button>
+        <button class="right border-0 rounded-1"><font-awesome-icon :icon="['fas', 'arrow-right']" /></button>
+      </section>
+    </div>
+
+  </header>
+</template>
 
 <style lang="scss" scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from "vue";
 import "./assets/scss/app.scss";
 import App from "./App.vue";
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
 
-createApp(App).mount("#app");
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import {
+  faArrowLeft,
+  faArrowRight,
+  faCartShopping,
+} from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+library.add(faCartShopping, faArrowLeft, faArrowRight);
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
