457178ee36f91efe2b625db6f12f0487bab8848d
DanieleMorello <danielemorello@live.it>
2023-04-27 12:41:33 +0200

    Complete TestimonialApp

--- src/components/TestimonialsComponent.vue
+++ src/components/TestimonialsComponent.vue
@@ -6,18 +6,25 @@ export default {
 
 <template>
   <div id="testimonials">
-    <a href="#"><img src="../assets/img/ebay-logotan-2x-200x66.png" alt="Ebay"></a>
-    <a href="#"><img src="../assets/img/audible-logotan-2x-200x66.png" alt="Audible"></a>
-    <a href="#"><img src="../assets/img/bb-logotan-2x-200x66.png" alt="Barnes"></a>
-    <a href="#"><img src="../assets/img/kindlefire-logotan-2x-200x66.png" alt="kindlefire"></a>
+    <div class="top text-center">
+      <a href="#"><img src="../assets/img/ebay-logotan-2x-200x66.png" alt="Logo Ebay"></a>
+      <a href="#"><img src="../assets/img/audible-logotan-2x-200x66.png" alt=" LogoAudible"></a>
+      <a href="#"><img src="../assets/img/bb-logotan-2x-200x66.png" alt=" Logo Barnes"></a>
+      <a href="#"><img src="../assets/img/kindlefire-logotan-2x-200x66.png" alt="Logo kindlefire"></a>
+    </div>
+
+    <div class="bottom">
+      <div>
+        <h3>New Audiobook</h3>
+        <h4>NO TIME TO READ? NO PROBLEM. LISTEN TO IT</h4>
+        <p>
+          Donec fringilla nunc eu turpis dignissim, at euismod<br>
+          sapien tincidunt. Maecenas eleifend congue cursus.
+        </p>
+        <img src="../assets/img/audible-logo.png" alt="Logo Audible">
+        <img class="pe-3" src="../assets/img/kindle-logo-tall.png" alt="Logo kindlefire">
+        <img src="../assets/img/kindle-logo-tall.png" alt="Logo kindlefire">
+      </div>
+    </div>
   </div>
 </template>
-
-<style lang="scss" scoped>
-@use '../assets/scss/partials/variables' as *;
-
-#testimonials {
-  background-color: $DawnPink;
-  padding: 3rem 0;
-}
-</style>
