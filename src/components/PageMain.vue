1c6b5d49cbd7961b51f08979be3488721dbb9b4f
DanieleMorello <danielemorello@live.it>
2023-04-27 08:33:01 +0200

    Add TestimonialsComponent

--- src/components/PageMain.vue
+++ src/components/PageMain.vue
@@ -1,12 +1,14 @@
 <script>
 import AboutComponent from './AboutComponent.vue';
+import TestimonialsComponent from './TestimonialsComponent.vue';
 
 export default {
   name: "PageMain",
-  components: { AboutComponent }
+  components: { AboutComponent, TestimonialsComponent }
 }
 </script>
 
 <template>
   <AboutComponent></AboutComponent>
+  <TestimonialsComponent></TestimonialsComponent>
 </template>
---
+++ src/components/TestimonialsComponent.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: "TestimonialsComponent",
+}
+</script>
+
+<template>
+  <section id="testimonials">
+    <a href="#"><img src="../assets/img/ebay-logotan-2x-200x66.png" alt="Ebay"></a>
+    <a href="#"><img src="../assets/img/audible-logotan-2x-200x66.png" alt="Audible"></a>
+    <a href="#"><img src="../assets/img/bb-logotan-2x-200x66.png" alt="Barnes"></a>
+    <a href="#"><img src="../assets/img/kindlefire-logotan-2x-200x66.png" alt="kindlefire"></a>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/partials/variables' as *;
+
+#testimonials {
+  background-color: $DawnPink;
+  padding: 3rem 0;
+}
+</style>
