360f7409b851cde8c128dd9684fd17ea8c4e9091
DanieleMorello <danielemorello@live.it>
2023-04-27 08:35:01 +0200

    chane section to div

--- src/components/TestimonialsComponent.vue
+++ src/components/TestimonialsComponent.vue
@@ -5,12 +5,12 @@ export default {
 </script>
 
 <template>
-  <section id="testimonials">
+  <div id="testimonials">
     <a href="#"><img src="../assets/img/ebay-logotan-2x-200x66.png" alt="Ebay"></a>
     <a href="#"><img src="../assets/img/audible-logotan-2x-200x66.png" alt="Audible"></a>
     <a href="#"><img src="../assets/img/bb-logotan-2x-200x66.png" alt="Barnes"></a>
     <a href="#"><img src="../assets/img/kindlefire-logotan-2x-200x66.png" alt="kindlefire"></a>
-  </section>
+  </div>
 </template>
 
 <style lang="scss" scoped>
